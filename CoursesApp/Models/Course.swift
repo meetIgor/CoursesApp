2a29673d30bdf70b0ce2e3abebeb697821b56be1
meetIgor <iigoor.s@gmail.com>
2022-10-22 17:35:34 +0500

    add Course Model

---
+++ CoursesApp/Models/Course.swift
@@ -0,0 +1,15 @@
+//
+//  Course.swift
+//  CoursesApp
+//
+//  Created by igor s on 22.10.2022.
+//
+
+import Foundation
+
+struct Course {
+    let name: String
+    let imageUrl: String
+    let numberOfLessons: Int
+    let numberOfTests: Int
+}
